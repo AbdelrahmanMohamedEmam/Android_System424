cfc91ab76be7269205f091dca20b9e1cb0b69bf1
farah8599 <Farah_amr8@yahoo.com>
2020-03-16 11:07:38 +0200

    added main screens of library

---
+++ spotify/lib/Screens/Library/artists_screen.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ArtistsScreen extends StatelessWidget {
+  void chooseArtistsButton() {
+    print('button pressed');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        child: FlatButton.icon(
+            onPressed: chooseArtistsButton,
+            icon: Icon( 
+              Icons.control_point,
+              color: Colors.grey,
+              size: 40,
+            ),
+            label: Text(
+              'Choose artists',
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            )),
+      ),
+    );
+  }
+}
