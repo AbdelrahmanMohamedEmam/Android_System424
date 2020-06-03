cfaa5c06bb632196489f027e8afa7c0e895f7662
Abdelrahman Emam <abdelrahmanemam91@gmail.com>
2020-06-04 00:58:56 +0200

    Recent activity finished fully paginated

--- spotify/lib/API_Providers/notificationAPI.dart
+++ spotify/lib/API_Providers/notificationAPI.dart
@@ -6,6 +6,7 @@ import 'package:spotify/Models/http_exception.dart';
 class NotificationsEndPoints {
   static const me = '/me';
   static const notifications = '/notifications';
+  static const limit = '?limit=8';
 }
 
 class NotificationsAPI {
@@ -14,14 +15,15 @@ class NotificationsAPI {
     this.baseUrl,
   });
   String nextNotifications;
-  String get getNextNotificationUrl{
+  String get getNextNotificationUrl {
     return nextNotifications;
   }
 
   Future<List> fetchNotificationsAPI(String token) async {
     final url = baseUrl +
         NotificationsEndPoints.me +
-        NotificationsEndPoints.notifications;
+        NotificationsEndPoints.notifications+
+        NotificationsEndPoints.limit;
     try {
       final response = await http.get(
         url,
@@ -31,6 +33,28 @@ class NotificationsAPI {
         Map<String, dynamic> temp = json.decode(response.body);
         nextNotifications = temp['data']['results']["next"];
         final extractedList = temp['data']['results']['items'] as List;
+        return extractedList;
+      } else {
+        print(response.statusCode);
+        throw HttpException(json.decode(response.body)['message'].toString());
+      }
+    } catch (error) {
+      throw HttpException(error.toString());
+    }
+  }
+
+  Future<List> fetchMoreNotificationsAPI(String token, String nextUrl) async {
+    final url = nextUrl;
+    try {
+      final response = await http.get(
+        url,
+        headers: {"authorization": "Bearer " + token},
+      );
+      if (response.statusCode == 200) {
+        Map<String, dynamic> temp = json.decode(response.body);
+        nextNotifications = temp['data']['results']["next"];
+        final extractedList = temp['data']['results']['items'] as List;
+        nextNotifications = temp['data']['results']["next"];
         return extractedList;
       } else {
         print(response.statusCode);
